b0d54a346847e6f4f54a64ff5dcaf9695f1a3a2e
Daniel Rogahn <draquix@yahoo.com>
2021-12-01 09:34:18 -0600

    generates map and tiles with character sprite movement

--- js/game.js
+++ js/game.js
@@ -4,6 +4,9 @@ const tileSize = 64;
 const numTiles = 9;
 const uiWidth = 4;
 
+spritesheet = new Image();
+spritesheet.src = 'js/spritesheet.png';
+
 function setupCanvas(){
     canvas = document.querySelector("canvas");
     ctx = canvas.getContext("2d");
@@ -13,7 +16,6 @@ function setupCanvas(){
     canvas.style.height = canvas.height + 'px';
 }
 
-ctx.fillRect(0,0,20,20,20);
 
 
 document.querySelector("html").onkeypress = function(e){
@@ -22,10 +24,25 @@ document.querySelector("html").onkeypress = function(e){
     if(e.key=="a") x--;
     if(e.key=="d") x++;
 };
-
+function drawSprite(sprite,x,y){
+    ctx.drawImage(
+        spritesheet,
+        sprite*16,
+        0,
+        16,
+        16,
+        x*tileSize,
+        y*tileSize,
+        tileSize,
+        tileSize
+    );
+}
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillRect (x*tileSize,y*tileSize,tileSize,tileSize);
+    for(let i=0;i<numTiles;i++){
+        for(let j=0;j<numTiles;j++){
+            getTile(i,j).draw();
+        }
+    }
+    drawSprite(0,x,y);
 }
-
-setInterval(draw, 15);
